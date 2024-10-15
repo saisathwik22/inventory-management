606ce742dc446c78133da25025e9299667716dc5
saisathwik22 <reddysaisathwik022@gmail.com>
2024-10-15 14:52:00 +0530

    Popular Products Card issue solved and merged CodeStage - 2:47:00

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -24,7 +24,7 @@ app.use((0, cors_1.default)());
 /* ROUTES */
 app.use("/dashboard", dashboardRoutes_1.default); // http://localhost:8000/dashboard
 /* SERVER */
-const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server running on port ${port}`);
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -22,7 +22,7 @@ app.use(cors());
 app.use("/dashboard", dashboardRoutes); // http://localhost:8000/dashboard
 
 /* SERVER */
-const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
